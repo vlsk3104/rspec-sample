a49fe4094a62f692b9d005e503731995d0c3777b
vlsk3104 <vlsk3104@gmail.com>
2021-02-05 12:42:15 +0900

    モデルのテストコードを書く(年齢確認)

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -73,4 +73,49 @@ RSpec.describe FoodEnquete, type: :model do
       end
     end
   end
+
+  describe 'アンケート回答時の条件' do
+    context '年齢を確認すること' do
+      it '未成年はビール飲み放題を選択できないこと' do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 19,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        expect(enquete_sato).not_to be_valid
+        expect(enquete_sato.errors[:present_id]).to include(I18n.t('activerecord.errors.models.food_enquete.attributes.present_id.cannot_present_to_minor'))
+      end
+
+      it '成人はビール飲み放題を選択できないこと' do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 20,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        expect(enquete_sato).to be_valid
+      end
+    end
+  end
+
+  describe '#adult?' do
+    it '20歳未満は成人ではないこと' do
+      foodEnquete = FoodEnquete.new
+
+      expect(foodEnquete.send(:adult?, 19)).to be_falsey
+    end
+
+    it '20歳以上は成人であること' do
+      foodEnquete = FoodEnquete.new
+
+      expect(foodEnquete.send(:adult?, 20)).to be_truthy
+    end
+  end
 end
